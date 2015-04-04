e4e8d49355c1b1976ba720be46bf8a15310d0166
amsourav <souravsarkar@icloud.com>
2015-04-04 19:44:21 +0000

    making it pretty

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,15 +1,7 @@
 class Movie < ActiveRecord::Base
-<<<<<<< HEAD
-=======
-
->>>>>>> 458f5c3
   belongs_to :user
   has_attached_file :image, :styles => { :medium => "300x300"}
   validates_attachment_content_type :image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
   has_many :reviews
-<<<<<<< HEAD
   ratyrate_rateable "visual_effects", "original_score", "costume_design", "direction", "acting" 
-=======
-  ratyrate_rateable 'visual_effects', 'original_score', 'director', 'costume_design'
->>>>>>> 458f5c3
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   resources :movies do
-  	resources :reviews, except: [:show, :index]
+    resources :reviews, except: [:show, :index]
   end
   root 'movies#index'
 end
