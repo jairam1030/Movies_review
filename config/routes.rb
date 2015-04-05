c7e06920f11146e4d887b9d57dcfdd0ce8e69e56
sourav <souravsrkr254@gmail.com>
2015-04-05 11:13:48 +0530

    some changes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   resources :movies do
-    resources :reviews, except: [:show, :index]
+    resources :reviews, except: [:show, :index, :edit]
   end
   root 'movies#index'
 end
