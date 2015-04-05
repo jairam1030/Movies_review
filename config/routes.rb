17a878f62838cfd5454f9c4ac6129379750dd2ce
amsourav <souravsarkar@icloud.com>
2015-04-05 08:56:32 +0000

    searchkick install

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
+  
   post '/rate' => 'rater#create', :as => 'rate'
   devise_for :users
   resources :movies do
+    collection do
+      get 'search'
+    end
     resources :reviews, except: [:show, :index, :edit]
   end
   root 'movies#index'
