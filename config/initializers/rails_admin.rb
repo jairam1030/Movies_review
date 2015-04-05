aa2863182455db47e10c2320a2776dabf32da06c
sourav <souravsrkr254@gmail.com>
2015-04-05 11:35:16 +0530

    added admin! yaay!!!

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -1,20 +1,11 @@
 RailsAdmin.config do |config|
 
-  ### Popular gems integration
-
-  ## == Devise ==
-  # config.authenticate_with do
-  #   warden.authenticate! scope: :user
-  # end
-  # config.current_user_method(&:current_user)
-
-  ## == Cancan ==
-  # config.authorize_with :cancan
-
-  ## == PaperTrail ==
-  # config.audit_with :paper_trail, 'User', 'PaperTrail::Version' # PaperTrail >= 3.0.0
-
-  ### More at https://github.com/sferik/rails_admin/wiki/Base-configuration
+  config.authorize_with do
+    authenticate_or_request_with_http_basic('Login required') do |username, password|
+      username == Rails.application.secrets.user &&
+      password == Rails.application.secrets.password
+    end
+  end
 
   config.actions do
     dashboard                     # mandatory
